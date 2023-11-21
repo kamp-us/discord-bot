97ce0e724c5336fe39bc875c208a7137e8c88f3f
Can Sirin <8138047+cansirin@users.noreply.github.com>
2023-11-21 00:06:16 -0800

    ignore config.ts

---
+++ config.example.ts
@@ -0,0 +1,6 @@
+export const GUILD_ID = "guildID";
+export const CLIENT_ID = "clientID";
+export const DISCORD_TOKEN = "discordToken";
+
+export const NEXTAUTH_URL = "";
+export const CHATGPT_API_KEY = "";
