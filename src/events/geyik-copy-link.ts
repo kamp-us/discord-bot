e03d8375c051543a626b22bd11fb03476cee0b43
Murat-tunca <murattunca3@gmail.com>
2024-02-13 01:34:19 +0300

    Bot command to catch returning links in Geyik chats

--- src/events/geyik-copy-link.ts
+++ src/events/geyik-copy-link.ts
@@ -1,42 +1,29 @@
-import { Client, GatewayIntentBits, Partials, TextChannel } from "discord.js";
+import { Events, Message, TextChannel  } from "discord.js";
+import { client } from "../../createDiscordClient";
 import { VADIDEKI_GEYIK_CHANNEL_ID, VADIDEKI_LINK_CHANNEL_ID } from "../../config";
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessages,
-  ],
-  partials: [Partials.User],
-});
 
+const geyikLinkCopyHandler = {
+  name: Events.MessageCreate,
+  async execute(message: Message) {
+    if (message.channel.id === VADIDEKI_GEYIK_CHANNEL_ID) {
+      const linkRegex = /(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\- ?=%.]+/g;
+      const links = message.content.match(linkRegex);
 
-let dailyThread;
+      if (links) {
+        const belirlenenKanal = client.channels.cache.get(VADIDEKI_LINK_CHANNEL_ID) as TextChannel;
 
-
-client.on("messageCreate", async (message) => {
-  if (message.channelId === VADIDEKI_GEYIK_CHANNEL_ID) {
-    if (message.content.includes("http") || message.content.includes("www")) {
-      const belirlenenKanal = client.channels.cache.get(VADIDEKI_LINK_CHANNEL_ID) as TextChannel;
-
-      if (belirlenenKanal) {
-        
-        if (!dailyThread) {
-          const today = new Date();
-          const options = {
-            month: "long",
-            day: "numeric",
-          } as Intl.DateTimeFormatOptions;
-          const formattedDate = new Intl.DateTimeFormat("tr-TR", options).format(today);
-
-          dailyThread = await belirlenenKanal.threads.create({
-            name: `Links for ${formattedDate}`,
-            autoArchiveDuration: 1440, 
+        if (belirlenenKanal) {
+          links.forEach((link) => {
+            belirlenenKanal.send(`Geyik Chatteki Link: ${link}`);
           });
+        } else {
+          console.error("Belirlenen kanal bulunamadı.");
         }
-
-        dailyThread.send(`${message.author.tag} Geyik Kanalındaki Link: ${message.content}`);
       }
     }
-  }
-});
+  },
+};
+
+
+export default geyikLinkCopyHandler;
